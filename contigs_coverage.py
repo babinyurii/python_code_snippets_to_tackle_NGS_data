6e53b1e7987178b00009b469b1c8f5747a3818eb
Yuriy Babin <babin.yurii@gmail.com>
2018-03-14 15:45:11 +0300

    Add files via upload

---
+++ contigs_coverage.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 14 14:39:14 2018
+
+@author: yuriy
+"""
+
+from Bio import SeqIO
+from Bio.SeqUtils import GC
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+from math import log2
+sns.set()
+
+
+
+def contigs_cover_spades(file_name_or_path):
+    """takes fasta file (spades tool output) 
+    name or its path as an argument. 
+    creates two plots:
+    1. distribution of GC content in contigs
+    2. plots GC content vs log2 coverage depth """
+    
+    container = []
+    
+    # using seq_record obj to access its attributes: 
+    # id - name, seq - nucleotides
+    for seq_record in SeqIO.parse(file_name_or_path, "fasta"):
+        entry = ()
+        entry = (seq_record.id, GC(seq_record.seq))
+        container.append(entry)
+        entry = ()
+      
+    gc = [x[1] for x in container]
+    
+    # fig1 = plt.figure()
+    fig1 = sns.distplot(gc, hist=False, kde_kws={"shade":True})
+    fig1.set_title("GC distribution")
+    
+    
+    reg = re.compile(r'cov_(.*)')
+    coverage = []
+    for el in container:
+        cov_raw = reg.search(el[0])
+        cov = cov_raw.group()
+        cov = cov[4:]
+        coverage.append(float(cov))
+    
+    cov_log2 = [log2(x) for x in coverage]
+    fig2 = plt.figure()
+    plt.scatter(gc, cov_log2, s=5)
+    plt.xlabel("GC content")
+    plt.ylabel("log2 coverage depth")
+    plt.title("contigs' coverage by reads")
+    plt.savefig("contigs_coverage_depth", format="jpeg")
+
+
+# function call
+contigs_cover_spades("/home/yuriy/sal/contigs_003.fasta")
+
+
+
+
+    
+    
+
+    
+    
+
+
+
