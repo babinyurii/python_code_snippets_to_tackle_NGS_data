3c4b2f657a2b7f2349c4c9f70cc713a05a893e5f
Yuriy Babin <babin.yurii@gmail.com>
2018-07-03 12:57:43 +0300

    Update seqman.py

--- seqman.py
+++ seqman.py
@@ -10,6 +10,7 @@ from Bio import SeqIO
 from Bio import AlignIO
 from Bio.SeqUtils import GC
 import subprocess
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import re
